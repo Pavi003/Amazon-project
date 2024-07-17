058a3a555caf1d3ef8f811b0628bc0734614579a
pavi273 <obialosilas@gmail.com>
2024-07-17 22:40:55 +0100

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,8 +81,16 @@ document.querySelectorAll('.js-add-to-cart')
             quantity: 1
         })
         }
+        let cartQuantity = 0;
 
-        console.log(cart)
+        cart.forEach((item) => {
+            cartQuantity += item.quantity
+        })
+
+        document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity
+
+        
         })
         
     })
